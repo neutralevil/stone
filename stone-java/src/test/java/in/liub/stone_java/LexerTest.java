9131994a307e53c63242152534b91925d85b12e9
neutralevil <liubin.net@gmail.com>
2015-11-07 21:24:55 +0800

    Add test support

---
+++ stone-java/src/test/java/in/liub/stone_java/LexerTest.java
@@ -0,0 +1,11 @@
+package in.liub.stone_java;
+
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+public class LexerTest {
+    @Test
+    public void shouldFail() {
+        // fail("fail expected");
+    }
+}
