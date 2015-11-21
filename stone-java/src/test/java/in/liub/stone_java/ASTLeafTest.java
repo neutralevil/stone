dd071b2b62d52a9fa85024c0ac8505a923a6701e
neutralevil <liubin.net@gmail.com>
2015-11-21 21:42:08 +0800

    Refactor ASTLeafTest

--- stone-java/src/test/java/in/liub/stone_java/ASTLeafTest.java
+++ stone-java/src/test/java/in/liub/stone_java/ASTLeafTest.java
@@ -1,5 +1,7 @@
 package in.liub.stone_java;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Iterator;
@@ -8,21 +10,29 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ASTLeafTest {
+    private ASTLeaf leaf;
+
+    @Before
+    public void setUp() {
+        this.leaf = new ASTLeaf();
+    }
+
+    @After
+    public void tearDown() {
+        this.leaf = null;
+    }
     @Test
     public void leafIsNoChild() {
-        ASTLeaf leaf = new ASTLeaf();
         assertThat(leaf.numChildren(), is(0));
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void canNotGetChildFromALeafNode() {
-        ASTLeaf leaf = new ASTLeaf();
         leaf.child(0);
     }
 
     @Test
     public void canNotIterateChildrenFromALeafNode() {
-        ASTLeaf leaf = new ASTLeaf();
         Iterator<ASTree> children = leaf.children();
         assertThat(children.hasNext(), is(not(true)));
     }
